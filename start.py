f5b8f235ba17c8bf7d13bf0f61add831320c3916
Bryce Bjork <brycedbjork@gmail.com>
2019-09-16 22:55:17 -0400

    working out

--- start.py
+++ start.py
@@ -4,4 +4,4 @@ path = os.path.dirname(os.path.realpath(__file__))
 
 os.system("DISPLAY=:1")
 os.system("processing-java --force --sketch="+path +
-          " --output="+os.getcwd()+"/out --run")
+          " --output="+path+"/out --run")
